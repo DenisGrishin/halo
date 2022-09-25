d03b9785e419ee349b2bb554a4d823e1283f7d88
Denis <666trall666@gmail.com>
2022-09-25 04:32:10 +0300

    Add files via upload

--- js/comebacker.js
+++ js/comebacker.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
-  (function (selectorForm, srcPath) {
+  (function (selectorForm, srcPath, heigthImg = '300') {
     comebacker(selectorForm, srcPath);
 
     function comebacker(formSelector) {
@@ -19,36 +19,40 @@ window.addEventListener('DOMContentLoaded', function () {
           location.href = linkBack;
         }
 
-        createModulWindow(srcPath);
+        createModulWindow(srcPath, heigthImg);
         localStorage.setItem('isShown', true);
       });
 
-      function createModulWindow(srcPath) {
+      function createModulWindow(srcPath, heigthImg) {
         const styleModal = `
         .modul-bg {
           padding: 20px;
-          display: none;
           width: 100%;
           height: 100%;
           position: fixed;
           top: 0;
           left: 0;
-          z-index: 100000;
+          visibility: hidden;
         }
         
         .modul-bg.active { 
+          z-index: 1000000;
           overflow: auto;
           display: flex;
           justify-content: center;
+        visibility: visible;
+
       }
      
       .modul-wrapper{
         padding: 15px;
         width: 100%;
         max-width: 600px;
+       
       }
      
       .modul {
+        border-radius: 20px;
         position: relative;
         display: flex;
         justify-content: center;
@@ -58,8 +62,12 @@ window.addEventListener('DOMContentLoaded', function () {
         padding: 55px 25px 25px 25px ;
         flex-direction: column;
     
+        transform: scale(0);
+        transition: transform 0.3s ease 0s;
       }
-      
+      .modul.active { 
+        transform: scale(1);
+    }
       .module-btn-close {
         font-size: 20px;
         position: absolute;
@@ -77,7 +85,7 @@ window.addEventListener('DOMContentLoaded', function () {
         color: #f91155;
       }
       .modul-img{
-        width: 350px;
+        width: ${heigthImg}px;
         
         max-width: 100%;
         margin-bottom: 20px;
@@ -85,35 +93,53 @@ window.addEventListener('DOMContentLoaded', function () {
       body {
         overflow: hidden;
         touch-action: none;
+        
       }
-      body.bg-show-popap {
-    
+      body::before{
+        content: "";
+        transition: all 0.3s ease 0s;
+        opacity:0;
+        visibility: hidden;
       }
- 
-.bg-show-popap::before {
-	content: "";
-	position: fixed;
-	width: 100%;
-	height: 100%;
-	top: 0;
-	z-index: 1;
-	left: 0;
-	background-color: rgba(0, 0, 0, 0.5);
-}
-      form{
+     
+      body.bg-show-modal::before {
+        content: "";
+        visibility: visible;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        z-index: 100000;
+        left: 0;
+        opacity:0.5;
+        background-color: rgba(0, 0, 0, 1);
+      }
+     
+      form{  
+        color: #000;
+        background-color: #fff !important;
         width:100% !important;
+      }
+      
+      input{
+        margin-top: 0.5rem !important;
+        padding: 0.625rem !important;
+        width: 100% !important;
+        border: 2px solid #000 !important;
+        outline: none !important;
+        background-color: #fff;
       }
       `;
 
-        let style = document.createElement('style');
-        const modulBg = document.createElement('div');
-        const modulWrapper = document.createElement('div');
-        const modul = document.createElement('div');
-        const btnClose = document.createElement('button');
-        const titleModul = document.createElement('h1');
-        const imgBlockModul = document.createElement('div');
-        const imgModul = document.createElement('img');
-        const form = document.querySelector(formSelector).cloneNode(true);
+        const modulBg = document.createElement('div'),
+          modulWrapper = document.createElement('div'),
+          modul = document.createElement('div'),
+          btnClose = document.createElement('button'),
+          titleModul = document.createElement('h1'),
+          imgBlockModul = document.createElement('div'),
+          imgModul = document.createElement('img'),
+          form = document.querySelector(formSelector).cloneNode(true),
+          style = document.createElement('style');
 
         imgBlockModul.classList.add('modul-block-img');
         modulBg.classList.add('modul-bg');
@@ -138,32 +164,54 @@ window.addEventListener('DOMContentLoaded', function () {
         modul.appendChild(imgModul);
         modul.appendChild(form);
         modulBg.appendChild(style);
-
-        showModal(modulBg, btnClose);
+        setTimeout(() => {
+          showModal(modulBg, btnClose, modul);
+        }, 1);
       }
 
-      function showModal(modulBg, btnClose) {
+      function showModal(modulBg, btnClose, modul) {
         if (modulBg) {
+          let scroll = calcScroll();
           modulBg.classList.add('active');
+          modul.classList.add('active');
 
-          document.body.classList.add('bg-show-popap');
-          document.addEventListener('click', (e) => {
-            if (e.target === modulBg) {
-              modulBg.classList.remove('active');
-              document.body.classList.remove('bg-show-popap');
-
-              localStorage.removeItem('isShown');
-              modulBg.remove();
-            }
-            if (e.target === btnClose) {
-              modulBg.classList.remove('active');
-              document.body.classList.remove('bg-show-popap');
-              localStorage.removeItem('show');
-              modulBg.remove();
-            }
-          });
+          document.body.classList.add('bg-show-modal');
+          document.body.style.paddingRight = `${scroll}px`;
+          closeModal(modulBg, btnClose, modul);
         }
       }
+      function closeModal(modulBg, btnClose, modul) {
+        document.addEventListener('click', (e) => {
+          if (e.target === modulBg || e.target === btnClose) {
+            modul.classList.remove('active');
+
+            localStorage.removeItem('isShown');
+            setTimeout(() => {
+              document.body.classList.remove('bg-show-modal');
+              modulBg.classList.remove('active');
+              modulBg.remove();
+              document.body.style.paddingRight = `0px`;
+            }, 200);
+          }
+        });
+      }
+      function calcScroll() {
+        let div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflow = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+        return scrollWidth;
+      }
     }
-  })('.form-order', 'http://127.0.0.1:5500/img/main/help-weight/01.jpg');
+  })(
+    '.form-order',
+    'https://funart.pro/uploads/posts/2022-06/1654756218_58-funart-pro-p-samii-malenkii-yezhik-v-mire-zhivotnie-kra-63.jpg',
+    '300'
+  );
 });
